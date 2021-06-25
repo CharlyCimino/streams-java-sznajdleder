4265068adb40131b4ee03eb60dca7a0b2559260d
Charly Cimino <carlosecimino@gmail.com>
2021-06-25 19:57:48 -0300

    Cierre de recursos en finally

--- src/streams/Principal.java
+++ src/streams/Principal.java
@@ -11,19 +11,34 @@ import java.io.PrintStream;
  */
 public class Principal {
 
-    public static void main(String[] args) throws Exception {
-        // abro el archivo y redirecciono la standar error
-        FileOutputStream fos = new FileOutputStream("errores.txt");
-        PrintStream stdErr = new PrintStream(fos);
-        System.setErr(stdErr);
-        int[] arr = new int[5];
-        // error cuando i sea mayor que 4
-        for (int i = 0; i < 10; i++) {
-            arr[i] = 0;
+    public static void main(String[] args) {
+        FileOutputStream fos = null;
+        PrintStream stdErr = null;
+        try {
+            // abro el archivo
+            fos = new FileOutputStream("errores.txt");
+            // instancio un printstream basado en el input stream
+            stdErr = new PrintStream(fos);
+            // seteo la estandard error
+            System.setErr(stdErr);
+            int[] arr = new int[5];
+            // error cuando i sea mayor que 4
+            for (int i = 0; i < 10; i++) {
+                arr[i] = 0;
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                if (stdErr != null) {
+                    stdErr.close();
+                }
+                if (fos != null) {
+                    fos.close();
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
         }
-        // cierro todo
-        stdErr.close();
-        fos.close();
     }
-
 }
